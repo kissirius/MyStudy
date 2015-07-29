97cb1fe10b39857bc217b291a306c2a2d3668b1f
kissirius <xianzonghuang@126.com>
2015-07-29 17:11:38 +0800

    commited 20150729

--- Visual Studio 2013/Projects/data_structure/data_structure/data_structure.c
+++ Visual Studio 2013/Projects/data_structure/data_structure/data_structure.c
@@ -1,65 +1,48 @@
 #include <stdio.h>
 
-#define MAXSIZE 10
+#define MAXSIZE 20
 
-#define OK 1
+#define OK    1
 #define ERROR 0
-#define TRUE 1
-#define FALSE 0
 
 typedef int Status;
 typedef int ElemType;
 
 typedef struct
 {
-	ElemType data[MAXSIZE];
+	int data[MAXSIZE];
 	int length;
-} SqList;
+
+}SqList;
 
 
-Status GetElem(SqList L, int i, ElemType * e)  // 获得元素操作
+// 获取链表元素
+Status GetElem(SqList L, int i, ElemType *e)
 {
-	if (L.length == 0 || i<1 || i >L.length) // 1. 链表长度等于0； 2. i（所处位置不能脱离其范围）；
+	if (L.length == 0 || i<1 || i>L.length)
 		return ERROR;
 	*e = L.data[i - 1];
 	return OK;
 }
 
-Status ListInsert(SqList * L, int i, ElemType e)
+Status ListInsert(SqList *L, int i, ElemType e)
 {
 	int k;
-	if (L->length == MAXSIZE)     // 顺序线表已经满
+	if (L->length == MAXSIZE)
 		return ERROR;
-	if (i <1 || i> L->length + 1)   // 插入的位置不在范围内
+	if (i < 1 || i>L->length)
 		return ERROR;
-
-	if (i < L->length)
+	if (i <= L->length)
 	{
 		for (k = L->length - 1; k >= i - 1; k--)
-		{
-			//                                             printf("L->data[%d] = L->data[%d]\n", L->data[k + 1], L->data[k]);
 			L->data[k + 1] = L->data[k];
-		}
 	}
-	L->data[i - 1] = e;
+	L->data[i - 1] = e; 
 	L->length++;
 	return OK;
 }
 
-void input_val(SqList * qlist)
+Status ListDelete(SqList *L, int i, ElemType *e)
 {
-	int i, j;
-	for (i = 0, j = 0; i < MAXSIZE; i++, j++)
-		scanf("%d", qlist->data[i]);
-
-	qlist->length = j;
-}
-
-int main()
-{
-	SqList qlist;
-
-	input_val(&qlist);
-
-	return 0;
+	
 }
